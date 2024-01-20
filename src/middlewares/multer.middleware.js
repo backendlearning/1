d46d63fddd73a18ebbde7dd81d90a7089dae6f5c
backendlearning <harshilathwaniofficial@proton.me>
2024-01-21 02:28:59 +0530

    Completed setup backend

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,9 +5,14 @@ const storage = multer.diskStorage({
       cb(null, './public/temp')
     },
     filename: function (req, file, cb) {
+    // changing file name
     //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.originalname)
     }
   })
   
-  const upload = multer({ storage: storage })
+  // export const upload = multer({ storage: storage })
+  //if using ES6
+  export const upload = multer({ 
+    storage, 
+  })
